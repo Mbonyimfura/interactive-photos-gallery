bc96b556802b43d259f474d8e3bd1224a511b507
Mbonyimfura <mbonyimfuratresor@gmail.com>
2024-06-08 10:16:29 +0200

    Add stringTransform.js

---
+++ challenges/stringTransform.js
@@ -0,0 +1,21 @@
+// challenge 2 transform string
+const transformString = (str) => {
+  let length = str.length;
+  if (length % 3 === 0 && length % 5 === 0) {
+    str = str.split("").reverse().join(""); // reverse the string
+    str = str
+      .split("")
+      .map((c) => c.charCodeAt(0))
+      .join(" "); // convert to ascii
+  } else if (length % 3 === 0) {
+    str = str.split("").reverse().join(""); // reverse the string
+  } else if (length % 5 === 0) {
+    str = str
+      .split("")
+      .map((c) => c.charCodeAt(0))
+      .join(" "); // convert to ascii
+  }
+  return str; // return the transformed string
+};
+//Example usage
+console.log(transformString("hello")); // 104 101 108 108 111
